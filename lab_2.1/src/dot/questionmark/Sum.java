f418da2677bfdae194f6b54a8d03ba038eb7d077
Kshitiz <kshitizsaini.rtk@gmail.com>
2020-02-23 23:14:18 +0530

    Lab Experiment 2

---
+++ lab_2.1/src/dot/questionmark/Sum.java
@@ -0,0 +1,10 @@
+package dot.questionmark;
+
+public class Sum {
+	
+	public static int addition(int num1, int num2)
+	{
+		return num1 + num2;
+	}
+
+}
---
+++ lab_2.1/src/dot/questionmark/TestSum.java
@@ -0,0 +1,14 @@
+package dot.questionmark;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestSum {
+
+	@Test
+	void testSum() {
+		assertEquals(5,Sum.addition(2,3));
+	}
+
+}
